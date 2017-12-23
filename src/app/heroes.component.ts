f9da363da78729619bfc956e7ecb1ada6ebaf8de
arindam.gohain <arindam.gohain@cgi.com>
2017-12-23 10:48:00 +0530

    First Commit

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-heroes',
+ 
+  styleUrls: ['./app.component.css']
+})
+export class HeroesComponent implements OnInit {
+  
+}
